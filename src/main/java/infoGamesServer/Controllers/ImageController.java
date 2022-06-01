33f0e31137001e85fd2decc6dc56c3056263653b
amishanov <amishanov@inbox.ru>
2022-06-01 13:43:31 +0300

    Update after images
    Сделал REST контроллер для картинок, осталось сделать в нём параметры

--- src/main/java/infoGamesServer/Controllers/ImageController.java
+++ src/main/java/infoGamesServer/Controllers/ImageController.java
@@ -1,20 +1,21 @@
 package infoGamesServer.Controllers;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
 public class ImageController {
 
-    @GetMapping("/v1/var/images/th2/")
-    public ResponseEntity<byte[]> getImage() {
-        InputStream in = getClass().getResourceAsStream("/var/images/th2/dc1.png");
-        IOUtils.
-        return IOUtils.(in);
-
+    @GetMapping(value = "/v1/var/images/th2/", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        InputStream in = getClass().getResourceAsStream("/images/th5/mux.png");
+        return new ResponseEntity <>(IOUtils.toByteArray(in), HttpStatus.OK);
     }
 }
